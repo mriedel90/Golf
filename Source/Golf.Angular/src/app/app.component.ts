8eebee3735569d7bfcc1451f1424140cff9c8214
Matt Riedel <dev6@sovren1.onmicrosoft.com>
2017-09-18 09:47:59 -0400

    angular

--- Source/Golf.Angular/src/app/app.component.ts
+++ Source/Golf.Angular/src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+
   title = 'app';
+
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get('/api/items').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+    });
+  }
+
 }
